56e5c73153e319abeb6a579f40637a79fc3a8f57
nitinshas3 <shastrin25@gmail.com>
2026-06-26 17:38:09 +0530

    initial commit , built the project structure , instead of using one model controller repository service , divided the structure into multiple features and in them created these packages seperately for each feature

---
+++ CodeForge/CodeForge/src/test/java/com/nitin/CodeForge/CodeForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitin.CodeForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
