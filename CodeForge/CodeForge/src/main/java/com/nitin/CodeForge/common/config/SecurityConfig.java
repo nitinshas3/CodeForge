80db78063a03fe6bb11d7c02dc6b67062951b0ba
nitinshas3 <shastrin25@gmail.com>
2026-06-29 23:49:07 +0530

    in security config modified security filter chain , created beans for autehtication manager and provider etc

--- CodeForge/CodeForge/src/main/java/com/nitin/CodeForge/common/config/SecurityConfig.java
+++ CodeForge/CodeForge/src/main/java/com/nitin/CodeForge/common/config/SecurityConfig.java
@@ -5,10 +5,16 @@ import com.nitin.CodeForge.common.config.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
@@ -22,11 +28,45 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    //@Bean
-   // public AuthenticationProvider authenticationProvider() {
-     //   DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-     //   authProvider.setUserDetailsService(customUserDetailService); // ✅ works if service implements UserDetailsService
-      //  authProvider.setPasswordEncoder(passwordEncoder());
-      //  return authProvider;
-   // }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(customUserDetailService);
+        //.setUserDetailsService(customUserDetailService); // ✅ works if service implements UserDetailsService
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    //look at this line later
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration)
+            throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+
+        http.csrf(csrf -> csrf.disable());
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        "/auth/**",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+        );
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(
+                jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class
+        );
+        return http.build();
+
+    }
 }
